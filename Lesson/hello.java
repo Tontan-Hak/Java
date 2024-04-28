c62cfa1ffca17c4224169bc3ebcc241cd37a58dc
Tontan Hak <tontanhak97@gmail.com>
2024-04-28 13:34:45 +0700

    Java testing code

--- Lesson/hello.java
+++ Lesson/hello.java
@@ -1,6 +1,6 @@
 public class hello {
 
     public static void main(String[] args) {
-        System.out.println("Thanks for writing!");
+        System.out.println("Learn to code in Java!");
     }
 }
